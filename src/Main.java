766939a53bb7654c3945277c78792fa4d9170524
IrinaIronya <bis_2002@mail.ru>
2019-11-23 18:12:28 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,54 @@
-
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;
 
-        int [] array = {54, 13, 7, 1, 10, 25, 3, 8, 17, 5};
+        if (low >= high)
+            return;
 
-        boolean isSorted = false;
-        int t;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < array.length-1; i++) {
-                if(array[i] > array [i+1]){
-                    isSorted = false;
 
-                    t = array[i];
-                    array [i] = array [i+1];
-                    array [i+1] = t;
-                }
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
+
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora) {
+                i++;
+            }
+
+            while (array[j] > opora) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
             }
         }
-        System.out.println(Arrays.toString(array));
+
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
+    }
+    public static void main(String[] args) {
+        int[] x = { 15, -7, 10, 7, 3, -2, 0, 12, 6 };
+        System.out.println("Было");
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(x));
     }
 }
