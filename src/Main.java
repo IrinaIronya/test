56fdc92b845792a388aff48c0e2045fd6ea0c20d
IrinaIronya <bis_2002@mail.ru>
2019-11-25 21:16:07 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,55 @@ import java.util.Arrays;
 
 public class Main {
 
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;
+
+        if (low >= high)
+            return;
+
+
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
+
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora) {
+                i++;
+            }
+
+            while (array[j] > opora) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
+    }
+    public static void main(String[] args) {
+        int[] x = { 15, -7, 10, 7, 3, -2, 0, 12, 6 };
+        System.out.println("Было");
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(x));
+    }
     public static void quickSort(long[] array, long low, long high) {
         if (array.length == 0)
             return;
@@ -39,7 +88,7 @@ public class Main {
         if (high > i)
             quickSort(array, i, high);
     }
-    public static void main(String[] args) {
+    public static void main2 (String[] args) {
         long[] x = { 15, -7, 10, 7, 3, -2, 0, 12, 6 };
         System.out.println("Было");
         System.out.println(Arrays.toString(x));
--- src/Main2.java
+++ src/Main2.java
@@ -2,7 +2,27 @@ import java.util.Arrays;
 
 public class Main2 {
 
-    public static void main(String[] args) {
+    public static void bubbleSort (String[] args) {
+
+        int [] array = {54, 13, 7, 1, 10, 25, 3, 8, 17, 5};
+
+        boolean isSorted = false;
+        int t;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i] > array [i+1]){
+                    isSorted = false;
+
+                    t = array[i];
+                    array [i] = array [i+1];
+                    array [i+1] = t;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    public static void bubbleSort2 (String[] args) {
 
         long [] array = {54, 13, 7, 1, 10, 25, 3, 8, 17, 5};
 
