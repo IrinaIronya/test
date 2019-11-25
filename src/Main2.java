3c023ccd75a3a883221ae3ef89c46e91a6056848
IrinaIronya <bis_2002@mail.ru>
2019-11-25 22:26:09 +0500

    дз

--- src/Main2.java
+++ src/Main2.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main2 {
 
-    public static void bubbleSort (String[] args, int b) {
+    public static void bubbleSort (int[] args, int b) {
 
         int [] array = {54, 13, 7, 1, 10, 25, 3, 8, 17, 5};
 
@@ -22,9 +22,10 @@ public class Main2 {
         }
         System.out.println(Arrays.toString(array));
     }
-    public static void bubbleSort (String[] args,long b) {
 
-        long [] array = {54, 13, 6, 1, 10, 25, 3, 8, 17, 5};
+    public static void bubbleSort (long[] args,long b) {
+
+        long [] array = {54, 13, 7, 1, 10, 25, 3, 8, 17, 5};
 
         boolean isSorted = false;
         long t;
